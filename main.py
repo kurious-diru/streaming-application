b155ff567d3f0cefb9f11534b9367410bf600083
diru1100 <rohitdandamudi.1100@gmail.com>
2021-04-25 23:05:37 +0530

    Add different approach

--- main.py
+++ main.py
@@ -3,33 +3,46 @@ import asyncio
 import time
 
 
+def stream_data(source):
+    # Loading data through generators, avoiding memory error
+    for record in pd.read_csv(source).to_dict('records'):
+        yield record
+
+
 async def main():
 
     begin = time.time()
     # take source input data
     source = "data/input/abalone_full.csv"
 
+    data = stream_data(source)
+
     sink1 = "data/output/infants_with_more_than_14_rings.csv"
     infant_processor = EnrichInfant(source, sink1)
-    # process data with given constraints on infant abalone
-    task1 = asyncio.create_task(
-        infant_processor.process_infants_with_more_than_14_rings())
 
     sink2 = "data/output/males_heavy_and_short.csv"
     males_processor = EnrichMale(source, sink2)
-    # process data with given constraints on male abalone
-    task2 = asyncio.create_task(
-        males_processor.process_males_heavy_and_short())
 
     sink3 = "data/output/shell_humidity.csv"
     shell_processor = FilterAbalone(source, sink3)
-    # process data with given constraints on all abalone
-    task3 = asyncio.create_task(shell_processor.process_shell_humidity())
+
+    for record in data:
+        # process data with given constraints on infant abalone
+        task1 = asyncio.create_task(
+            infant_processor.process_infants_with_more_than_14_rings(record))
+
+        # process data with given constraints on male abalone
+        task2 = asyncio.create_task(
+            males_processor.process_males_heavy_and_short(record))
+
+        # process data with given constraints on all abalone
+        task3 = asyncio.create_task(
+            shell_processor.process_shell_humidity(record))
 
     # awaiting till all 3 tasks are done asynchronously
-    value3 = await task3  # task3 probably has to process more records than other tasks
-    await task2
-    await task1
+        value3 = await task3  # task3 probably has to process more records than other tasks
+        value2 = await task2
+        value1 = await task1
     end = time.time()
 
     print(f"Time taken to run the program asynchronously is {end - begin}")
--- process.py
+++ process.py
@@ -18,12 +18,6 @@ class Processor(ABC):
                             "Shucked_weight", "Viscera_weight", "Shell_weight", "Class_number_of_rings"])
             file.close()
 
-    @abstractmethod
-    def stream_data(self):
-        # Loading data through generators, avoiding memory error
-        for record in pd.read_csv(self.source).to_dict('records'):
-            yield record
-
 
 class EnrichInfant(Processor):
 
@@ -34,33 +28,33 @@ class EnrichInfant(Processor):
         # Overriding ID with appropriate structure representing rings
         self.id = "R_"
 
-    def stream_data(self):
-        super().stream_data()
+    # def stream_data(self):
+    #     super().stream_data()
 
-    async def process_infants_with_more_than_14_rings(self):
-        data = super().stream_data()
+    async def process_infants_with_more_than_14_rings(self, record):
+        # data = super().stream_data()
         # Simulating each record feeding to the processor
-        for record in data:
-            # await asyncio.sleep(0.001)
-            rings = record["Class_number_of_rings"]
-            # Logic to consider appropiate records
-            if record["Sex"] == "I" and rings >= 14:
-                if rings in self.state:
-                    self.state[rings] = self.state[rings] + 1
-                else:
-                    self.state[rings] = 1
-                row_id = self.id
-                row_id = row_id + str(rings) + \
-                    "_" + str(self.state[rings])
+        # for record in data:
+        # await asyncio.sleep(0.001)
+        rings = record["Class_number_of_rings"]
+        # Logic to consider appropiate records
+        if record["Sex"] == "I" and rings >= 14:
+            if rings in self.state:
+                self.state[rings] = self.state[rings] + 1
+            else:
+                self.state[rings] = 1
+            row_id = self.id
+            row_id = row_id + str(rings) + \
+                "_" + str(self.state[rings])
 
-                list_record = list(record.values())
-                list_record.insert(0, row_id)
-                # Appeding the appropriate data with an extra ID attribute
-                with open(self.sink, 'a', newline='') as file:
-                    writer = csv.writer(file)
-                    writer.writerow(list_record)
-                    file.close()
-                # print("process1")
+            list_record = list(record.values())
+            list_record.insert(0, row_id)
+            # Appeding the appropriate data with an extra ID attribute
+            with open(self.sink, 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(list_record)
+                file.close()
+            # print("process1")
 
 
 class EnrichMale(Processor):
@@ -68,24 +62,24 @@ class EnrichMale(Processor):
     def __init__(self, source, sink):
         super().__init__(source, sink)
 
-    def stream_data(self):
-        super().stream_data()
+    # def stream_data(self):
+    #     super().stream_data()
 
-    async def process_males_heavy_and_short(self):
-        data = super().stream_data()
+    async def process_males_heavy_and_short(self, record):
+        # data = super().stream_data()
         # Simulating each record feeding to the processor
-        for record in data:
-            # Logic to consider appropiate records
-            if record["Sex"] == "M" and record["Whole_weight"] > 0.4 and record["Length"] < 0.5:
-                list_record = list(record.values())
-                list_record.insert(0, self.id)
-                self.id = self.id + 1
-                # Appeding the appropriate data with an extra ID attribute
-                with open(self.sink, 'a', newline='') as file:
-                    writer = csv.writer(file)
-                    writer.writerow(list_record)
-                    file.close()
-                # print("process2")
+        # for record in data:
+        # Logic to consider appropiate records
+        if record["Sex"] == "M" and record["Whole_weight"] > 0.4 and record["Length"] < 0.5:
+            list_record = list(record.values())
+            list_record.insert(0, self.id)
+            self.id = self.id + 1
+            # Appeding the appropriate data with an extra ID attribute
+            with open(self.sink, 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(list_record)
+                file.close()
+            # print("process2")
 
 
 class FilterAbalone(Processor):
@@ -99,28 +93,28 @@ class FilterAbalone(Processor):
                             "Shucked_weight", "Viscera_weight", "Shell_weight", "Class_number_of_rings", "Shell_humidity_weight"])
             file.close()
 
-    def stream_data(self):
-        super().stream_data()
+    # def stream_data(self):
+    #     super().stream_data()
 
-    async def process_shell_humidity(self):
-        data = super().stream_data()
+    async def process_shell_humidity(self, record):
+        # data = super().stream_data()
         # Simulating each record feeding to the processor
-        for record in data:
-            shell_humidity_weight = (
-                record["Whole_weight"] - record["Shucked_weight"]) - record["Shell_weight"]
-            shell_humidity_weight = round(shell_humidity_weight, 3)
-            # Logic to consider appropiate records
-            if shell_humidity_weight > 0:
-                list_record = list(record.values())
-                list_record.insert(0, self.id)
-                list_record.append(shell_humidity_weight)
-                self.id = self.id + 1
-                # Appeding the appropriate data with extra ID and Shell_humidity_weight attributes
-                with open(self.sink, 'a', newline='') as file:
-                    writer = csv.writer(file)
-                    writer.writerow(list_record)
-                    file.close()
-                # print("process3")
+        # for record in data:
+        shell_humidity_weight = (
+            record["Whole_weight"] - record["Shucked_weight"]) - record["Shell_weight"]
+        shell_humidity_weight = round(shell_humidity_weight, 3)
+        # Logic to consider appropiate records
+        if shell_humidity_weight > 0:
+            list_record = list(record.values())
+            list_record.insert(0, self.id)
+            list_record.append(shell_humidity_weight)
+            self.id = self.id + 1
+            # Appeding the appropriate data with extra ID and Shell_humidity_weight attributes
+            with open(self.sink, 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(list_record)
+                file.close()
+            # print("process3")
 
 
 if __name__ == '__main__':
